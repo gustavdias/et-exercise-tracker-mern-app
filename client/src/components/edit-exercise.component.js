7a6fd3f4c39d6689bcbcbf9a10c00c355f148531
gustavdias <gustav.almd@gmail.com>
2020-07-31 14:42:42 +0300

    client on netlify

--- client/src/components/edit-exercise.component.js
+++ client/src/components/edit-exercise.component.js
@@ -23,7 +23,7 @@ export default class EditExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+    axios.get('https://et-mern.herokuapp.com/exercises/'+this.props.match.params.id)
       .then(response => {
         this.setState({
           username: response.data.username,
@@ -86,7 +86,7 @@ export default class EditExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+    axios.post('https://et-mern.herokuapp.com/exercises/update/' + this.props.match.params.id, exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
